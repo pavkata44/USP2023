a03ef1b287359fff1434d1557d2c18f1376b24f4
shittyrestaurant@protonmail.com <shittyrestauraunt@protonmail.com>
2023-03-02 18:30:23 +0200

    pavka duhai

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("dasdas");
 		System.out.println("paraparaliiskitop");
+		System.out.println("dasdas");System.out.println("dasdas");System.out.println("dasdas");System.out.println("dasdas");System.out.println("dasdas");System.out.println("dasdas");System.out.println("dasdas");System.out.println("dasdas");
+
 	}
 
 }
